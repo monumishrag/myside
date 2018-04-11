43653dd80b528bb4fd2dfc789ed1c91e6a4fd99d
monumishrag <38273547+monumishrag@users.noreply.github.com>
2018-04-11 10:58:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,454 @@
+
+
+
+		
+		
+		   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>SAI SALE CORPORATION  | By AM SOFTWARE INDIA</title>
+    <meta charset="utf-8">
+
+    <!-- Linking styles -->
+    <link rel="stylesheet" href="csss/reset.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="csss/style.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="csss/nivo-slider.css" type="text/css" media="screen">
+
+    <!-- Linking scripts -->
+    <script src="jss/jquery.js"></script>
+    <script src="jss/jquery.nivo.slider.pack.js"></script>   
+    <script src="jss/main.js"></script>
+
+    <!--[if lt IE 9]>
+    <script type="text/javascript" src="js/html5.js"></script>
+    <![endif]-->
+
+<!-- ORDER NOW POP BOOT STP STYLE -->
+<style>
+#content{
+	background-color: 	#ffffe5;
+}
+
+</style>
+<!-- ORDER NOW POP BOOT STP STYLE -->
+	
+<!-- ORDER NOW POP BOOT STP STYLE -->
+
+ <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- / ORDER NOW POP BOOT STP STYLE -->	
+	
+	
+	</head>
+<body>
+<!-- ORDER NOW POP BOOT STP HTML -->
+
+<div class="container">
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">If you want to order </h4>
+        </div>
+        <div class="modal-body">
+          <p>
+		  <!-- ORDER notifacation text in html-->
+		  Thanking you for interest our product <br>please Contact our Company<br>
+ (1). 99263-77961<br>
+(2).  96170-29334
+<br>
+Email:- Saisalescorporation2@Gmail.Com
+
+<br>
+<br>                                 Thanking You 
+  		 
+		  </p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- ORDER NOW POP BOOT STP HTML -->
+
+
+
+
+
+
+
+<div class="container">
+
+        <header><!-- Defining the header section of the page -->
+
+            <nav><!-- Defining the navigation menu -->
+                <ul>
+                    <li class="selected"><a href="#">XXXX</a></li>
+                    
+                    <li><a onClick="passWord()" href="">extra</a></li>
+                </ul>
+            </nav>
+
+
+            <div class="top_head"><!-- Defining the top head element -->
+                <div class="logo"><!-- Defining the logo element -->
+                    <a href="#">
+                        <img src="images/logo.png" title="E-Store Sai sales" alt="E-Store template" />
+                    </a>
+                </div>
+
+                <section id="search"><!-- Search form -->
+                    <form action="#" onsubmit="return false;" method="get">
+                        <input type="text" onfocus="if (this.value =='Search..' ) this.value=''" onblur="if (this.value=='') this.value='Search..'" value="Search.." name="q">
+                        <input type="submit" value="Search">
+                    </form>
+
+                    <ul id="social"><!-- Social profiles links -->
+                        <li><a href="https://www.facebook.com/Sai-Sales-Corporation-550129465373020/?hc_ref=ARSk2iT5oUz71wW9e13L8M29KvX42pmBAc86DJ-d6ydEXpswjH6Jc6K2_UXZyWy0O8M&fref=nf" title="facebook" rel="external nofollow"><img alt="" src="images/facebook.png"></a></li>
+                        <li><a href="twwere" title="twitter" rel="external nofollow"><img alt="" src="images/twitter.png"></a></li>
+                        <li><a href="#" title="linkedin" rel="external nofollow"><img alt="" src="images/linkedin.png"></a></li>
+                        <li><a href="#" title="rss" rel="external nofollow"><img alt="" src="images/rss.png"></a></li>
+						<li><a href="goole plisde" title="Google" rel="external nofollow"><img alt="" src="images/Google.png"></a></li>
+                    </ul>
+                </section>
+            </div>
+
+            <section id="submenu"><!-- Defining the sub menu -->
+                <ul>
+                    <li><a href="">HOME</a></li>
+					<li><a href="searchproduct.php">All Product List By Search</a></li>
+                    <li><a href="aboutus.php#about">About Us</a></li>
+                    <li><a href="aboutus.php#services">services</a></li>
+                    <li><a href="aboutus.php#portfolio">portfolio</a></li>
+                    <li><a href="aboutus.php#pricing">pricing</a></li>
+					<li><a href="aboutus.php#contact">contact</a></li>
+                   
+                </ul>
+            </section>
+
+        </header>
+
+        <div id="slider"><!-- Defining the main content section -->
+
+        <!-- Promo slider -->
+            <section id="slider-wrapper">
+                <div id="slider" class="nivoSlider">
+                    <img style="display: none;" src="images/promo1.jpg" alt="" title="#htmlcaption-1">
+                    <img style="display: none;" src="images/promo2.jpg" alt="" title="#htmlcaption-2">
+                    <img style="display: none;" src="images/promo3.jpg" alt="" title="#htmlcaption-3">
+                </div>
+                <div id="htmlcaption-1" class="nivo-html-caption">
+                    <h5 class="p2">Welcome to the our E-Shop</h5>
+                    <p>Put any description here</p>
+                </div>
+                <div id="htmlcaption-1" class="nivo-html-caption">
+                    <h5 class="p2">This is promo area</h5>
+                    <p>Put any description here</p>
+                </div>
+                <div id="htmlcaption-2" class="nivo-html-caption">
+                    <h5 class="p2">Where you can add any feature products</h5>
+                    <p>Put any description here</p>
+                </div>
+                <div id="htmlcaption-3" class="nivo-html-caption">
+                    <h5 class="p2">Or something else</h5>
+                    <p>Put any description here</p>
+                </div>
+            </section>
+        </div>
+<br>
+        <div id="main"><!-- Defining submain content section -->
+            <section id="content"><!-- Defining the content section #2 -->
+                <div id="left">
+                    <h3>LIst products</h3>
+                    <ul>
+					
+					
+					
+					
+					
+	
+			        
+
+					   <li>					<!--page 1 me jakar php syntac ke andar  (echo $var = $_GET['id'];) likhe aur jaha par php qurry lagai h waha par ( WHERE id=$var";) likh de -->
+                            <div class="img"><a href="11.html"><img alt="" src="images/2.jpg"></a></div>
+                            <div class="info">
+                                <a class="title" href="11.html"><b>ZIG ZAG CUTTER</b></a>
+                                <p>Very fast production. Zigzag pieces also help fast</p>
+                                <div class="price">
+                                    <span class="st">Product ID:</span><strong>1</strong>
+                                </div>
+                                <div class="actions">
+                                    <a href="11.html">Details</a>
+                                    <a data-toggle="modal" data-target="#myModal" href="#">place order</a>
+                                </div>
+                            </div>
+                        </li>
+                       
+					   	
+			        
+
+					   <li>					<!--page 1 me jakar php syntac ke andar  (echo $var = $_GET['id'];) likhe aur jaha par php qurry lagai h waha par ( WHERE id=$var";) likh de -->
+                            <div class="img"><a href="12.html"><img alt="" src="images/3.jpg"></a></div>
+                            <div class="info">
+                                <a class="title" href="12.html"><b>TEFLON BELT</b></a>
+                                <p>Teflon Belt used in Band sealer Machine for sealin</p>
+                                <div class="price">
+                                    <span class="st">Product ID:</span><strong>2</strong>
+                                </div>
+                                <div class="actions">
+                                    <a href="12.html">Details</a>
+                                    <a data-toggle="modal" data-target="#myModal" href="#">place order</a>
+                                </div>
+                            </div>
+                        </li>
+                       
+					   	
+			        
+
+					   <li>					<!--page 1 me jakar php syntac ke andar  (echo $var = $_GET['id'];) likhe aur jaha par php qurry lagai h waha par ( WHERE id=$var";) likh de -->
+                            <div class="img"><a href="13.html"><img alt="" src="images/4.jpg"></a></div>
+                            <div class="info">
+                                <a class="title" href="13.html"><b>TENSION SPRING</b></a>
+                                <p>A heavy-duty helical spring designed for compressi</p>
+                                <div class="price">
+                                    <span class="st">Product ID:</span><strong>3</strong>
+                                </div>
+                                <div class="actions">
+                                    <a href="13.html">Details</a>
+                                    <a data-toggle="modal" data-target="#myModal" href="#">place order</a>
+                                </div>
+                            </div>
+                        </li>
+                       
+					   	
+			        
+
+					   <li>					<!--page 1 me jakar php syntac ke andar  (echo $var = $_GET['id'];) likhe aur jaha par php qurry lagai h waha par ( WHERE id=$var";) likh de -->
+                            <div class="img"><a href="14.html"><img alt="" src="images/5.jpg"></a></div>
+                            <div class="info">
+                                <a class="title" href="14.html"><b>Boiler brush</b></a>
+                                <p>This is boiler brush for candy machine</p>
+                                <div class="price">
+                                    <span class="st">Product ID:</span><strong>4</strong>
+                                </div>
+                                <div class="actions">
+                                    <a href="14.html">Details</a>
+                                    <a data-toggle="modal" data-target="#myModal" href="#">place order</a>
+                                </div>
+                            </div>
+                        </li>
+                       
+					   	
+			        
+
+					   <li>					<!--page 1 me jakar php syntac ke andar  (echo $var = $_GET['id'];) likhe aur jaha par php qurry lagai h waha par ( WHERE id=$var";) likh de -->
+                            <div class="img"><a href="15.html"><img alt="" src="images/7.jpg"></a></div>
+                            <div class="info">
+                                <a class="title" href="15.html"><b>CARTAGE HEATERS</b></a>
+                                <p>High density & Low density</p>
+                                <div class="price">
+                                    <span class="st">Product ID:</span><strong>5</strong>
+                                </div>
+                                <div class="actions">
+                                    <a href="15.html">Details</a>
+                                    <a data-toggle="modal" data-target="#myModal" href="#">place order</a>
+                                </div>
+                            </div>
+                        </li>
+                       
+					   	
+			        
+
+					   <li>					<!--page 1 me jakar php syntac ke andar  (echo $var = $_GET['id'];) likhe aur jaha par php qurry lagai h waha par ( WHERE id=$var";) likh de -->
+                            <div class="img"><a href="16.html"><img alt="" src="images/6.jpg"></a></div>
+                            <div class="info">
+                                <a class="title" href="16.html"><b>TIMING BELT</b></a>
+                                <p>This is a automotive camshaft belt for all machine</p>
+                                <div class="price">
+                                    <span class="st">Product ID:</span><strong>6</strong>
+                                </div>
+                                <div class="actions">
+                                    <a href="16.html">Details</a>
+                                    <a data-toggle="modal" data-target="#myModal" href="#">place order</a>
+                                </div>
+                            </div>
+                        </li>
+                       
+					   	
+			        
+
+					   <li>					<!--page 1 me jakar php syntac ke andar  (echo $var = $_GET['id'];) likhe aur jaha par php qurry lagai h waha par ( WHERE id=$var";) likh de -->
+                            <div class="img"><a href="17.html"><img alt="" src="images/8.jpg"></a></div>
+                            <div class="info">
+                                <a class="title" href="17.html"><b>TEFLON HEATING TAPE</b></a>
+                                <p>The coated fabric has a silicone pressure sensitiv</p>
+                                <div class="price">
+                                    <span class="st">Product ID:</span><strong>7</strong>
+                                </div>
+                                <div class="actions">
+                                    <a href="17.html">Details</a>
+                                    <a data-toggle="modal" data-target="#myModal" href="#">place order</a>
+                                </div>
+                            </div>
+                        </li>
+                       
+					   	
+			        
+
+					   <li>					<!--page 1 me jakar php syntac ke andar  (echo $var = $_GET['id'];) likhe aur jaha par php qurry lagai h waha par ( WHERE id=$var";) likh de -->
+                            <div class="img"><a href="18.html"><img alt="" src="images/9.jpg"></a></div>
+                            <div class="info">
+                                <a class="title" href="18.html"><b>PERFORATION BLADE</b></a>
+                                <p>These blades are made from HSS (High Speed steel)</p>
+                                <div class="price">
+                                    <span class="st">Product ID:</span><strong>8</strong>
+                                </div>
+                                <div class="actions">
+                                    <a href="18.html">Details</a>
+                                    <a data-toggle="modal" data-target="#myModal" href="#">place order</a>
+                                </div>
+                            </div>
+                        </li>
+                       
+					   	
+			        
+
+					   <li>					<!--page 1 me jakar php syntac ke andar  (echo $var = $_GET['id'];) likhe aur jaha par php qurry lagai h waha par ( WHERE id=$var";) likh de -->
+                            <div class="img"><a href="19.html"><img alt="" src="images/10_3.jpg"></a></div>
+                            <div class="info">
+                                <a class="title" href="19.html"><b>Bottle Brush</b></a>
+                                <p>Ideal for Slim Neck and Wide Neck Bottles</p>
+                                <div class="price">
+                                    <span class="st">Product ID:</span><strong>9</strong>
+                                </div>
+                                <div class="actions">
+                                    <a href="19.html">Details</a>
+                                    <a data-toggle="modal" data-target="#myModal" href="#">place order</a>
+                                </div>
+                            </div>
+                        </li>
+                       
+					   	
+			        
+
+					   <li>					<!--page 1 me jakar php syntac ke andar  (echo $var = $_GET['id'];) likhe aur jaha par php qurry lagai h waha par ( WHERE id=$var";) likh de -->
+                            <div class="img"><a href="20.html"><img alt="" src="images/11.jpg"></a></div>
+                            <div class="info">
+                                <a class="title" href="20.html"><b>PULLING TIMING BELT</b></a>
+                                <p>The draw down belts offered by us are rubber coate</p>
+                                <div class="price">
+                                    <span class="st">Product ID:</span><strong>10</strong>
+                                </div>
+                                <div class="actions">
+                                    <a href="20.html">Details</a>
+                                    <a data-toggle="modal" data-target="#myModal" href="#">place order</a>
+                                </div>
+                            </div>
+                        </li>
+                       
+					      
+					   
+					   
+					   
+					   
+					   
+					   
+					   
+					   
+                    </ul>
+                </div>
+                <div id="right">
+                    <h3>Top sells</h3>
+                    <ul>
+					
+					
+					
+								
+					
+  		
+						
+						
+						
+						
+						
+						
+						
+						
+                    </ul>
+                </div>
+            </section>
+        </div>
+
+<br>
+    <footer><!-- Defining the footer section of the page -->
+        <div id="privacy">
+            E-Store template © 2011 <a class="link" href="http://www.script-tutorials.com/">Privacy Policy</a><br />
+            <a class="link" href="http://www.script-tutorials.com/creating-new-html5css3-single-page-layout-e-store/">Template by Script Tutorials</a>
+        </div>
+    </footer>
+
+</div>
+
+<div style='text-align: right;position: fixed;z-index:9999999;bottom: 0; width: 100%;cursor: pointer;line-height: 0;display:block !important;'><a title="000webhost logo" rel="nofollow" target="_blank" href="https://www.000webhost.com/free-website-sign-up?utm_source=000webhostapp&amp;utm_campaign=000_logo&amp;utm_campaign=ss-footer_logo&amp;utm_medium=000_logo&amp;utm_content=website"><img src="https://cdn.rawgit.com/000webhost/logo/e9bd13f7/footer-powered-by-000webhost-white2.png" alt="000webhost logo"></a></div></body>
+
+<!-- diseble click -->
+
+<!-- diseble click -->
+
+
+<!-- password admin-->
+ <SCRIPT>
+function passWord() {
+    var testV = 1;
+    var pass1 = prompt('Add secrets codes ','durgesh gupta');
+    while (testV < 3) {
+        if (!pass1) history.go(-1);
+        if (pass1.toLowerCase() == "mishraginputme") {
+            alert('You Got it Right!');
+            window.open('ADMIN-add_product.php');
+            break;
+        } 
+        testV+=1;
+        var pass1 = prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+    }
+    if (pass1.toLowerCase()!="password" & testV ==3) history.go(-1);
+    return " ";
+}
+</SCRIPT>
+<!-- password admin-->
+
+</html>
